0b368416f43a99e2d0f0a3b416f6e698b8dff9ec
iamusayev <iamusayev1337@gmail.com>
2022-07-22 20:36:07 +0400

    added EnversImplementationApplication

---
+++ envers-implementation/src/main/java/com/example/enversimplementation/EnversImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.example.enversimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnversImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnversImplementationApplication.class, args);
+    }
+
+}

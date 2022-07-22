6b257bde8f6d8e86d6000894873f209153aacf3c
iamusayev <iamusayev1337@gmail.com>
2022-07-22 20:35:43 +0400

    added BookController

---
+++ envers-implementation/src/main/java/com/example/enversimplementation/controller/BookController.java
@@ -0,0 +1,38 @@
+package com.example.enversimplementation.controller;
+
+import com.example.enversimplementation.dao.entity.Book;
+import com.example.enversimplementation.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+
+
+    @PostMapping("/saveBook")
+    public Book saveBook(@RequestBody Book book) {
+        return bookService.save(book);
+    }
+
+
+    @PutMapping("/update/{id}/{pages}")
+    public String updateBook(@PathVariable Integer id, @PathVariable Integer pages) {
+        return bookService.updateBook(id, pages);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public String deleteBook(@PathVariable Integer id) {
+        return bookService.deleteBook(id);
+    }
+
+
+    @GetMapping("/getInfo/{id}")
+    public void getInfo(@PathVariable Integer id) {
+        bookService.getInfo(id);
+    }
+
+}
